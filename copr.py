e92c060d10055975895dc1a3ac696aa2a626d10c
Sud0Us3r <ivandezad@gmail.com>
2024-03-20 14:23:15 +0100

    Added copr.py

--- copr.py
+++ copr.py
@@ -10,14 +10,27 @@ def mcd(a, b):
 def esCoprimo(num1, num2):
     return mcd(num1, num2) == 1 #Devolvemos true si y solo si mcd = 1, si no devolveremos 0
 
+def calcularInversos(num, lista1, lista2):
+    for num1 in lista1:
+        for num2 in lista2:
+            if (num1 * num2 % num) == 1:
+                print(f"Encontrado una pareja de inversos multiplicativos: {num1}*{num2} (mod{num}) = 1")
+
+
+
 def calcularCoprimos(num):
+    lista = []
     for i in range(1, num):
         if esCoprimo(i, num):
-            print(f"Found coprimo con {num}: {i}")
+            print(f"Encontrado coprimo con {num}: {i}")
+            lista.append(i)
+    return lista
 
 def main():
     num = int(input("Ingrese el número para calular sus coprimos: "))
-    calcularCoprimos(num)
+    list1 = calcularCoprimos(num)
+    list2 = list(list1)
+    calcularInversos(num, list1, list2)
 
 if __name__ == '__main__':
     main()
